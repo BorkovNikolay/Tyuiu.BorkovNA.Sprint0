b719a5d58e6ed165ee76b0d63e31ba25f4666a4e
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-09-13 17:20:46 +0500

    Task1

---
+++ Tyuiu.BorkovNA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tyuiu.BorkovNA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Nikolay";
+            int age = 20;
+            Console.WriteLine("Hello, my name is " + name + ". I am " + age + " years.");
+            Console.ReadKey();
+
+        }
+    }
+
+}
