3524b5dee811aa3b1f78b904cbdfb71e463eb1d6
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-09-15 19:26:34 +0500

    Task7

---
+++ Tyuiu.BorkovNA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.BorkovNA.Sprint0.Task7.V0.Lib
+{
+    public class DataServise
+    {
+        public static int[] AdditionArrays(int[] numsOne, int[] numsTwo)
+        {
+            int[] resultArray = new int[5];
+
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numsOne[i] + numsTwo[i];
+            }
+            return resultArray;
+        }
+
+    }
+}
