be536cc45276e9c90d98a619853b290257514424
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-09-15 17:58:45 +0500

    Task5

---
+++ Tyuiu.BorkovNA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Tyuiu.BorkovNA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.BorkovNA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B ="+DataServise.Addition(1, 5));
+            Console.WriteLine("A - B =" + DataServise.Subtraction(15, 5));
+            Console.WriteLine("A * B =" + DataServise.Multiplication(10, 10));
+            Console.WriteLine("A / B =" + DataServise.Division(9, 3));
+            Console.ReadKey();
+
+        }
+    }
+
+}
